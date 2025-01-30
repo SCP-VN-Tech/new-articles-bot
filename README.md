5480572bca4f739759bab0dc92ad01e66057dad6
wolf20482 <wolf20482@proton.me>
2025-01-30 19:36:56 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# New Articles Bot
+This is a Node.js program used by the SCP-VN Wiki to automatically send notifications about new articles on a specific Wikidot wiki to a Discord webhook.
+
+## Installation
+
+1. Make sure you have a Wikidot account that you can use for this program. It is recommended to create a Wikidot account separate from your current account, for use with this program. Only account creation is necessary; there is no need for the account to join the wiki that you're tracking for new articles, nor any need to grant it special rights. 
+    
+    Supplying Wikidot credentials to the program is for bypassing Wikidot's static cache, which prevents timely updates.
+2. Clone this repository.
+3. Rename the `config.json.example` file to `config.json`, and fill in required information (self-explanatory).
+4. Install necessary npm packages by running `npm install`.
+5. Run the program using `node .` and leave it on, preferably by hosting it on a server (such as a VPS).
+
+## Limitations
+This program's current design only allows it to process one article at a time. Therefore, the polling interval used is very short (only 5 seconds, which is also related to the author's original intention to make updates as close to real-time as possible). If the polling interval is too long (for instance, 1 hour), and during that time there is more than one new article, the program may miss the other articles and only send a notification on the latest article. Rewriting the program to allow for more than one new article within the polling interval is currently not a planned feature.
+
+## License
+This program is licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,96 @@
+const config = require('./config.json');
+const cheerio = require('cheerio');
+const { request } = require('undici');
+
+let latestPage = null;
+let wikidotAuth = null;
+
+async function fetchLatestPage() {
+    await request(`http://${config.siteName}.wikidot.com/feed/pages/pagename/most-recently-created/category/_default%2Cadult%2Ctheme%2Ccomponent%2Ccomponent-backend/tags/-admin%2C-b%C3%A0n-l%C3%A0m-vi%E1%BB%87c/rating/%3E%3D-10/order/created_at+desc/limit/30/t/Most+Recently+Created`, {
+        headers: {
+            'Cache-Control': 'max-age=0',
+            'Cookie': wikidotAuth,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+        }
+    }).then(async res => {
+        if (res.statusCode != 200) return console.error(`Wikidot error: Status code ${res.statusCode}`);
+        if (res.headers['x-wikidot-static-cache'] != 'BYPASS') console.warn(`Warning: Not bypassing Wikidot static cache (x-wikidot-static-cache=${res.headers['x-wikidot-static-cache']})`);
+        try {
+            const Parser = require('rss-parser');
+            const parser = new Parser({
+                customFields: {
+                    item: ['description']
+                }
+            });
+            await parser.parseString(await res.body.text()).then(async feed => {
+                let newPage = feed.items[0];
+                if (newPage.link == latestPage) return;
+
+                latestPage = newPage.link;
+
+                let $ = cheerio.load(newPage.description);
+
+                const payload = {
+                    content: null,
+                    embeds: [
+                        {
+                            title: newPage.title,
+                            description: `Đăng lúc: <t:${Math.floor(new Date(newPage.isoDate).getTime() / 1000)}:F>\nBởi người dùng: [${$('p > span > a:nth-child(2)').text()}](${$('body > p > span > a:nth-child(2)').attr('href')})`,
+                            url: newPage.link,
+                            color: 5814783,
+                            author: {
+                                name: 'Bài mới trên Wiki SCP-VN'
+                            }
+                        }
+                    ],
+                    attachments: []
+                };
+
+                try {
+                    const response = await request(config.webhookUrl, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(payload)
+                    });
+
+                    if (response.statusCode != 204) {
+                        console.error(`Failed to send message. Status: ${response.statusCode}`);
+                    }
+                } catch (error) {
+                    console.error('Error sending message to Discord:', error);
+                }
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    });
+    setTimeout(fetchLatestPage, 5000);
+}
+
+// login to bypass wikidot cache
+async function loginToWikidot() {
+    const wikidotToken7 = Math.random().toString(36).substring(4);
+    const [username, password] = config.wdLogin.split(':');
+    try {
+        const response = await request('https://www.wikidot.com/default--flow/login__LoginPopupScreen', {
+            method: 'POST',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+                'Cookie': `wikidot_token7=${wikidotToken7}`,
+            },
+            body: `login=${username}&password=${encodeURIComponent(password)}&action=Login2Action&event=login`
+        });
+
+        if ((await response.body.text()).includes('The login and password do not match.')) throw new Error('Invalid Wikidot username or password.');
+
+        wikidotAuth = `${response.headers['set-cookie'].split('; ')[0]}; wikidot_token7=${wikidotToken7}`;
+        console.log('Successfully logged in to Wikidot. Monitoring new articles');
+    } catch (err) {
+        throw new Error(err);
+    }
+    setTimeout(fetchLatestPage, 1000);
+}
+
+loginToWikidot();
